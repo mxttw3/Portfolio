1af5659498a17a8f673ae4e327356189cbe334d5
mxttw <matthew.a.c.04@gmail.com>
2024-12-19 18:18:56 +0100

    finished exp

--- src/pages/exp/exp.jsx
+++ src/pages/exp/exp.jsx
@@ -10,7 +10,32 @@ const Exp = () => {
                 </div>
             </section>
             <section className="expSection2">
-
+                <div className="paddingTopExp">
+                    <div className="timeLine1">
+                        <div className="round"></div>
+                        <div className="textCubeExp">
+                            <p className="timeTittle"><a>Z</a>aibr Innovations (Berlin)</p>
+                            <p className="timeText">5 Months focused on AI-driven web application development.</p>
+                        </div>
+                    </div>
+                    <div className="timeLine1" id="paddingTopExp">
+                        <div className="round"></div>
+                        <div className="textCubeExp">
+                            <p className="timeTittle"><a>W</a>ebsite for Conregfluid (Terrassa)</p>
+                            <p className="timeText">Currently developing a new website for a solenoid valve business.</p>
+                        </div>
+                    </div>
+                    <div className="timeLine1" id="paddingTopExp">
+                        <div className="round"></div>
+                        <div className="textCubeExp">
+                            <p className="timeTittle"><a>W</a>ebsite for Nicstorm (Berlin)</p>
+                            <p className="timeText">Creative portfolo website for a music producer, <a href="https://www.nicstorm.com">nicstorm.com</a></p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="positionExp">
+                <a href="https://www.linkedin.com/in/matthew-angulo-caballero/" className="linkedin">{'< FIND ME ON LINKEDIN />'}</a>
             </section>
         </main>
     )
