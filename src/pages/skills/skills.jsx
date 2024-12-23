6371143200223d691ea14f8545bbdf743501629f
mxttw <matthew.a.c.04@gmail.com>
2024-12-23 08:46:03 +0100

    skills page

--- src/pages/skills/skills.jsx
+++ src/pages/skills/skills.jsx
@@ -1,9 +1,31 @@
 import "./skills.css";
+import jsLogo from "../../components/images/jsLogo.png";
+import phpLogo from "../../components/images/phpLogo.png";
+import reactLogo from "../../components/images/reactLogo.png";
+import nextLogo from "../../components/images/nextLogo.png";
+import nodeLogo from "../../components/images/nodeLogo.png";
+import sqlLogo from "../../components/images/sqlLogo.png";
+import mongoLogo from "../../components/images/mongoLogo.png";
+import figmaLogo from "../../components/images/figmaLogo.png";
 
 const Skills = () => {
     return (
         <main className="mainGrid5">
-            <p>In progress...</p>
+            <section className="skillsContainer">
+                <div className='centerSkills'>
+                    <div className="logosContainer">
+                        <img src={jsLogo} alt="" className="logoImage" id='jsLogo'/>
+                        <img src={phpLogo} alt="" className="logoImage" id='phpLogo' />
+                        <img src={reactLogo} alt="" className="logoImage" id='reactLogo'/>
+                        <img src={nextLogo} alt="" className="logoImage" id='nextLogo' />
+                        <img src={nodeLogo} alt="" className="logoImage" id='nodeLogo'/>
+                        <img src={sqlLogo} alt="" className="logoImage" id='sqlLogo'/>
+                        <img src={mongoLogo} alt="" className="logoImage" id='mongoLogo'/>
+                        <img src={figmaLogo} alt="" className="logoImage" id='figmaLogo'/>
+                    </div>
+                    <h2 className="skillsTitle">SKILLS</h2>
+                </div>
+            </section>
         </main>
     )
 };
